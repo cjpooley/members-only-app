352e61bc880f9f1dc271a5db535d1ec14f0093a2
Chris Pooley <cpooley@gmail.com>
2021-04-22 17:00:07 -0700

    Tons of updates

--- src/groups/CreateGroupPage.js
+++ src/groups/CreateGroupPage.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { postWithCredentials } from '../data';
 
-export const CreateGroupPage = () => (
-  <h1>This is the Create Group Page</h1>
-);
+export const CreateGroupPage = () => {
+  const [nameValue, setNameValue] = useState('');
+  const history = useHistory();
+
+  const createGroup = async () => {
+    const response = await postWithCredentials('/groups', { name: nameValue });
+    const { newGroupId } = await response.json();
+    history.push(`/groups/${newGroupId}`);
+  }
+  
+  return (
+    <div className="centered-container">
+      <h1>Create Group</h1>
+      <input
+        type="text"
+        placeholder="Enter name for group"
+        value={nameValue}
+        onChange={e => setNameValue(e.target.value)} />
+      <button onClick={createGroup}>Create Group</button>
+    </div>
+  );
+}
--- src/groups/GroupPage.js
+++ src/groups/GroupPage.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useUser } from '../auth';
+import { useProtectedResource, postWithCredentials } from '../data';
+import { MessagesList } from '../messages';
+import { RequestsList } from '../requests';
 
-export const GroupPage = () => (
-  <h1>This is the Group Page</h1>
-);
+export const GroupPage = () => {
+  const [messageValue, setMessageValue] = useState('');
+  const { id } = useParams();
+  const { user } = useUser();
+  const { data: group, setData: setGroup } = 
+    useProtectedResource(`/groups/${id}`, { owner: {}, messages: [], requests: [], });
+
+  console.log(group);
+  
+  return (
+    <div className="centered-container">
+      <h1>{group.name}</h1>
+      <p>Owned by: {group.owner.fullName}</p>
+      <MessagesList messages={group.messages} />
+      <div className="new-message-form">
+        <input
+          type="text"
+          placeholder="Type your message here..."
+          value={messageValue}
+          onChange={e => setMessageValue(e.target.value)} />
+        <button>Submit</button>
+      </div>
+    </div>
+  )
+}
